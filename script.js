65ef4cc7d9dd08fb5fce5f7e1c6aaabbe3162fd4
Mohd Saif <126072969+SA1FUU@users.noreply.github.com>
2023-11-16 12:58:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let paraPrimes = document.getElementById("para-primes")
 
 let clearBtn = document.getElementById("clear-btn")
 let downloadBtn = document.getElementById("downloadButton")
+let copyBtn = document.getElementById("copyButton")
 let input1 = document.querySelector("#number1")
 let input2 = document.querySelector("#number2")
 
@@ -26,11 +27,13 @@ function isPrime(num) {
     return true;
 }
 
+
 // For Validating the Input and Generating the Prime Number for the Value Provided
 // Also for Adding and Removing Few Elements or Their Contents Accordingly 
 
 const startInput = document.getElementById('number1');
 const endInput = document.getElementById('number2');
+
 
 function generatePrimes() {
     const errorMessage = document.getElementById('noti');
@@ -40,22 +43,23 @@ function generatePrimes() {
     const end = parseInt(endInput.value);
 
 
-
     if (isNaN(start) || isNaN(end)) {
         errorMessage.innerText = "Enter a Valid Number"
         // Custom
         sourceCode.style.visibility = "hidden"
         downloadBtn.style.display = "none"
+        copyBtn.style.display = "none"
         paraPrimes.innerText = ""
         primeOutput.innerText = ""
         return
     }
 
-    if(start > end) {
+    if (start > end) {
         errorMessage.innerText = "FIrst Number Should be Less than Second Number"
         // Custom
         sourceCode.style.visibility = "hidden"
         downloadBtn.style.display = "none"
+        copyBtn.style.display = "none"
         paraPrimes.innerText = ""
         primeOutput.innerText = ""
         return
@@ -63,56 +67,72 @@ function generatePrimes() {
 
     if (!Number.isInteger(start) || start < 0) {
         errorMessage.innerText = "Please enter a positive integer."
-         // Custom
-         sourceCode.style.visibility = "hidden"
-         downloadBtn.style.display = "none"
-         paraPrimes.innerText = ""
-         primeOutput.innerText = ""
+        // Custom
+        sourceCode.style.visibility = "hidden"
+        downloadBtn.style.display = "none"
+        copyBtn.style.display = "none"
+        paraPrimes.innerText = ""
+        primeOutput.innerText = ""
         return;
     }
 
     if (!Number.isInteger(end) || end < 0) {
         errorMessage.innerText = "Please enter a positive integer."
-         // Custom
-         sourceCode.style.visibility = "hidden"
-         downloadBtn.style.display = "none"
-         paraPrimes.innerText = ""
-         primeOutput.innerText = ""
+        // Custom
+        sourceCode.style.visibility = "hidden"
+        downloadBtn.style.display = "none"
+        copyBtn.style.display = "none"
+        paraPrimes.innerText = ""
+        primeOutput.innerText = ""
         return;
     }
 
     if (start > 9999999999 || end > 9999999999) {
         errorMessage.innerText = "Number should be up to 10 digits."
-         // Custom
-         document.querySelector("section").style.visibility = "hidden"
-         downloadBtn.style.display = "none"
-         paraPrimes.innerText = ""
-         primeOutput.innerText = ""
+        // Custom
+        document.querySelector("section").style.visibility = "hidden"
+        downloadBtn.style.display = "none"
+        copyBtn.style.display = "none"
+        paraPrimes.innerText = ""
+        primeOutput.innerText = ""
         return;
     }
-    
+
     else {
         const primes = [];
         for (let num = start; num <= end; num++) {
+
             if (isPrime(num)) {
                 primes.push(num);
             }
 
             if (primes.length > 0) {
                 paraPrimes.innerText = "Prime Numbers Found Between  " + start + " and " + end + " Are  "
-                primeOutput.innerText =   primes.join(", ")
+                primeOutput.innerText = primes.join("  ")
                 errorMessage.innerText = ""
-            } 
+            }
             else {
-                primeOutput.innerText = "No prime number found between " + start + " and " + end
+                paraPrimes.innerText = "No prime number found between " + start + " and " + end
                 errorMessage.innerText = ""
             }
         }
     }
-   
-    document.querySelector("section").style.visibility = "visible"
-    downloadBtn.style.display = "block"
+    // For Copying the Prime Numbers to the Clipboard
 
+    copyBtn.addEventListener("click", copyToClipboard)
+
+    function copyToClipboard() {
+        var copyPrimes = primeOutput.innerText;
+        navigator.clipboard.writeText(copyPrimes)
+        copyBtn.disabled  = true
+        copyBtn.textContent = "Copied"
+    }
+
+    document.querySelector("section").style.visibility = "visible"
+    copyBtn.textContent = "Copy to Clipboard"
+    copyBtn.disabled = false
+    downloadBtn.style.display = "block"
+    copyBtn.style.display = "block"
 }
 
 
@@ -120,21 +140,23 @@ function generatePrimes() {
 
 clearBtn.addEventListener("click", () => {
     document.getElementById("downloadButton").style.display = "none"
+    document.getElementById("copyButton").style.display = "none"
     document.querySelector("section").style.visibility = "hidden"
-    input1.value = ""
-    input2.value = ""
+    copyBtn.disabled  = false
+    copyBtn.textContent = "Copy to Clipboard"
+    paraPrimes.innerText = ""
     para.innerText = ""
     noti.innerText = ""
-    paraPrimes.innerText = ""
+    input1.value = ""
+    input2.value = ""
 })
-
 
 
 // For Downloading the Conetent in Text Format on Clicking Save Button
 
 document.getElementById("downloadButton").addEventListener("click", function () {
 
-    var textContent =  document.querySelector("#para").innerText
+    var textContent = document.querySelector("#para").innerText
 
     var blob = new Blob([textContent], { type: "text/plain" });
 
@@ -149,13 +171,11 @@ document.getElementById("downloadButton").addEventListener("click", function () 
 });
 
 
-
 // / Key Code for First Input Button to Shift Focus on Second Input
 
 input1.addEventListener("keydown", function (event) {
-    if (event.keyCode === 39 || event.keyCode === 40) {
-        input1.focus() === false
-        input2.focus() = true
+    if (event.keyCode === 40) {
+        input2.focus()
     }
 });
 
@@ -166,6 +186,5 @@ input2.addEventListener("keypress", function (event) {
         document.getElementById("generatePrimes").click();
     }
 });
-
 
 
